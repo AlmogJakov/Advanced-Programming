a3223c9ec206b3af6958ffd8ae5ba7f0b968d3e9
AlmogJakov <almog1006@gmail.com>
2022-03-21 18:27:16 +0200

    first commit

--- Task 1/main.c
+++ Task 1/main.c
@@ -13,72 +13,115 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
-//#include "utmplib.c"
-
-#define SHOWHOST
-
-
-#define NRECS 16
-#define NULLUT ((struct utmp * )NULL)
 #define UTSIZE (sizeof(struct utmp))
+#define NULLUT ((struct utmp * )NULL)
 
-static char utmpbuf[NRECS *UTSIZE];     /*storage*/
-static int num_recs;        /* num stored */
-static int cur_rec;         /* next to go*/
-static int fd_utmp;         /* read from */
-struct utmp *first;
+int fd;
+int cur_rec = 0;
+static char utmpbuf[UTSIZE];
 
-utmp_open( char * filename ){
-    fd_utmp = open( filename, O_RDONLY );       /*open it */
-    cur_rec = num_recs = 0;     /* no records yet */
-    return fd_utmp;     /*report*/
+
+int new_wtmp_fd() {
+    int new_fd = open( "/var/log/wtmp", O_RDONLY );
+        if (new_fd == -1) {
+            perror("/var/log/wtmp");
+            exit(1);
+    }
+    return new_fd;
 }
 
-struct utmp *utmp_next(){
-    struct utmp *recp;
-    if (fd_utmp == -1 )
-        return NULLUT;
-    if (cur_rec == num_recs && utmp_reload() == 0)
-        return NULLUT;
-
-    /* get adress of next record */
-
-    recp = (struct utmp * ) &utmpbuf[cur_rec * UTSIZE];
-    cur_rec++;
-    return recp;
+void logoutDetails(struct utmp *ut) {
+    if(ut->ut_type == BOOT_TIME) {
+        int bool = 0;
+        strcpy(ut->ut_line, "system boot");
+        //printf("[%-8.8s]",ut->ut_user);
+        struct utmp *p;
+        struct timeval temp_tv;
+        int temp_fd = new_wtmp_fd();
+        lseek (temp_fd, cur_rec * UTSIZE, SEEK_CUR);
+        char temp_utmpbuf[UTSIZE];
+        while (bool < 1 && (read(temp_fd, temp_utmpbuf, UTSIZE) > 0)) {
+            p = (struct utmp * ) &temp_utmpbuf;
+            if(strncmp(p->ut_user,"shutdown",8)==0) {
+                temp_tv.tv_sec = p->ut_tv.tv_sec;
+                temp_tv.tv_usec = p->ut_tv.tv_usec;
+                time_t rawtime = temp_tv.tv_sec;
+                printf ( "\n[%-8.8s] time: %s", p->ut_user, ctime (&rawtime) );
+                //strcpy(p->ut_user, "used");
+                //(printf)(" [%ld]",(4 + ctime (&temp_tv.tv_sec),(long int) temp_tv.tv_usec));
+                bool++;
+            }
+        }
+        if (bool==0) {
+            printf ("crashed");
+        }
+    }
+    printf ("\n");
 }
 
-int utmp_reload(){
-    /* 
-     * read next bunch of records into buffer
-     */
-    int amt_read;
-    amt_read = read (fd_utmp, utmpbuf, NRECS * UTSIZE);
-    num_recs = amt_read/UTSIZE;
-    cur_rec = 0;
-    return num_recs;
-}
 
-utmp_close(){
-    if ((fd_utmp )!= -1)
-        close( fd_utmp);
+void show_info(struct utmp *utbufp) {
+    struct utmp *temp = utbufp;
+    struct timeval temp_tv;
+    temp_tv.tv_sec = utbufp->ut_tv.tv_sec;
+    temp_tv.tv_usec = utbufp->ut_tv.tv_usec;
+    (printf) (
+    /* The format string.  */
+    #if _HAVE_UT_TYPE
+            "[%d] "
+    #endif
+    #if _HAVE_UT_PID
+            "[%05d] "
+    #endif
+    #if _HAVE_UT_ID
+            "[%-4.4s] "
+    #endif
+            "[%-8.8s] [%-12.12s]"
+    #if _HAVE_UT_HOST
+            " [%-16.16s]"
+    #endif
+            " [%-15.15s]"
+    #if _HAVE_UT_TV
+            " [%ld]"
+    #endif
+            "" // "\n"
+            /* The arguments.  */
+    #if _HAVE_UT_TYPE
+            , utbufp->ut_type
+    #endif
+    #if _HAVE_UT_PID
+            , utbufp->ut_pid
+    #endif
+    #if _HAVE_UT_ID
+            , utbufp->ut_id
+    #endif
+            , utbufp->ut_user, utbufp->ut_line
+    #if _HAVE_UT_HOST
+            , utbufp->ut_host
+    #endif
+    #if _HAVE_UT_TV
+            , 4 + ctime (&temp_tv.tv_sec)
+            , (long int) temp_tv.tv_usec
+    #else
+            , 4 + ctime (&utbufp->ut_time)
+    #endif
+           );
+logoutDetails(temp);
 }
-
-void show_time(long);
-void show_info(struct utmp*);
 
 int main() {
-    struct utmp *utbufp,          // holds pointer to next record
-                *utmp_next();     // returns pointer to next
-
-    if (utmp_open("/var/log/wtmp") == -1) {
-        perror("/var/log/wtmp");
-        exit(1);
+    fd = new_wtmp_fd();
+    //printf("utbufp size = %d\n", (int)sizeof(struct utmp));
+    while ((read(fd, utmpbuf, UTSIZE) > 0)) {
+        show_info((struct utmp * ) &utmpbuf);
+        cur_rec++;
     }
-    first = utbufp;
-    printf("utbufp size = %d\n", (int)sizeof(struct utmp));
-    while ((utbufp = utmp_next()) != ((struct utmp*)NULL)) show_info(utbufp);
-    utmp_close();
+    // lseek (fd, UTSIZE, SEEK_CUR);
+    // memset(fd, 0, UTSIZE);
+
+    //read (fd, currRecord, UTSIZE);
+    //show_info(currRecord);
+    printf("hey\n");
     return 0;
 }
 
@@ -86,82 +129,6 @@ int main() {
 
 
 
-struct utmp *utmp_get(int record){
-    struct utmp *recp;
-    if (fd_utmp == -1 )
-        return NULLUT;
-    if (record == num_recs && utmp_reload() == 0)
-        return NULLUT;
-
-    /* get adress of next record */
-
-    recp = (struct utmp * ) &utmpbuf[record * UTSIZE];
-    return recp;
-}
-
-
-
-void logoutDetails(struct utmp *ut) {
-    int c = 0;
-    int whydown = 0;
-    time_t lastboot = 0;
-    int quit = 0;
-    if(ut->ut_type == USER_PROCESS) {
-        printf("[%-8.8s]",ut->ut_user);
-        /*
-         *  This was a login - show the first matching
-         *  logout record and delete all records with
-         *  the same ut_line.
-         */
-        struct utmp *p = utmp_get(0);
-        struct timeval temp_tv;
-        for (int i = 0; i < num_recs; i++) {
-            p = utmp_get(i);
-            if ((strncmp(p->ut_user,"shutdown",8)==0) && (strncmp(p->ut_line, ut->ut_line, UT_LINESIZE) == 0)) {
-                /* Show it */
-                if (c == 0) {
-                    //quit = list(&ut, p->ut_time, R_NORMAL);
-                    
-                    temp_tv.tv_sec = p->ut_tv.tv_sec;
-                    temp_tv.tv_usec = p->ut_tv.tv_usec;
-                    time_t rawtime = temp_tv.tv_sec;
-                    printf ( "[%-8.8s] time: %s", p->ut_user, ctime (&rawtime) );
-                    strcpy(p->ut_user, "used");
-                    //(printf)(" [%ld]",(4 + ctime (&temp_tv.tv_sec),(long int) temp_tv.tv_usec));
-                    c = 1;
-                }
-                // if (p->next) p->next->prev = p->prev;
-                // if (p->prev)
-                //     p->prev->next = p->next;
-                // else
-                //     //utmplist = p->next;
-                // free(p);
-            }
-        }
-        
-        //time_t rawtime = temp_tv.tv_sec;
-        //printf ( "[%-8.8s] Current date and time are: %s", p->ut_user, ctime (&rawtime) );
-        /*
-         *  Not found? Then crashed, down, still
-         *  logged in, or missing logout record.
-         */
-        // if (c == 0) {
-        //     if (lastboot == 0) {
-        //         c = R_NOW;
-        //         /* Is process still alive? */
-        //         if (p->ut_pid > 0 &&
-        //             kill(p->ut_pid, 0) != 0 &&
-        //             errno == ESRCH)
-        //             c = R_PHANTOM;
-        //     } else
-        //         c = whydown;
-        //     //quit = list(&ut, lastboot, c);
-        // }
-    }
-    if (c==0) {
-            printf ("crashed\n");
-    }
-}
 
 
 
@@ -169,85 +136,12 @@ void logoutDetails(struct utmp *ut) {
 
 
 
-void show_info(struct utmp *utbufp) {
-    struct utmp *temp = utbufp;
-    
-    //printf("%.*s\n" , (int)(sizeof utbufp->ut_id), utbufp->ut_id);
-/*
- *  displays the contents of the utmp struct
- *  in human readable form
- *  displays nothing if record has no user name
- */
-    // if (utbufp->ut_type != USER_PROCESS)
-    //     return;
-    
-    // printf("% -10.10s", utbufp->ut_name); /* the log name*/
-    // printf(" ");    
-    ///////////////////////////////
-    struct timeval temp_tv;
-  temp_tv.tv_sec = utbufp->ut_tv.tv_sec;
-  temp_tv.tv_usec = utbufp->ut_tv.tv_usec;
-    (printf) (
-            /* The format string.  */
-#if _HAVE_UT_TYPE
-            "[%d] "
-#endif
-#if _HAVE_UT_PID
-            "[%05d] "
-#endif
-#if _HAVE_UT_ID
-            "[%-4.4s] "
-#endif
-            "[%-8.8s] [%-12.12s]"
-#if _HAVE_UT_HOST
-            " [%-16.16s]"
-#endif
-            " [%-15.15s]"
-#if _HAVE_UT_TV
-            " [%ld]"
-#endif
-            "" // "\n"
-            /* The arguments.  */
-#if _HAVE_UT_TYPE
-            , utbufp->ut_type
-#endif
-#if _HAVE_UT_PID
-            , utbufp->ut_pid
-#endif
-#if _HAVE_UT_ID
-            , utbufp->ut_id
-#endif
-            , utbufp->ut_user, utbufp->ut_line
-#if _HAVE_UT_HOST
-            , utbufp->ut_host
-#endif
-#if _HAVE_UT_TV
-            , 4 + ctime (&temp_tv.tv_sec)
-            , (long int) temp_tv.tv_usec
-#else
-            , 4 + ctime (&utbufp->ut_time)
-#endif
-           );
-    ///////////////////////////////
-    // printf("% -8.8s", utbufp->ut_line); /* the tty */
-    // printf(" ");
-    // show_time(utbufp->ut_time);
-// #ifdef SHOWHOST
-//     if (utbufp->ut_host[0] != '\0')
-//         printf("( %s)", utbufp->ut_host);
-// #endif
-logoutDetails(temp);
-}
 
-// void show_time(long timeval) {
-// /*
-//  * displays time in a format fit for human consumption
-//  * uses ctime to build a string then picks parts out of it
-//  */
-//     char *cp;   /* to hold address of time*/
-//     cp = ctime(&timeval); /* convert time to string */
-//     printf("% 12.12s", cp+4);
-// } 
+
+
+
+
+
 
 
 
