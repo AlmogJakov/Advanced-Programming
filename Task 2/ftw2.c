2f8484a4875fd46956136b95879da6ef8bd87f17
AlmogJakov <almog1006@gmail.com>
2022-04-11 17:43:51 +0300

    'getNumOfFiles' method implementation.

--- Task 2/ftw2.c
+++ Task 2/ftw2.c
@@ -18,12 +18,32 @@
 #include <string.h>       /* Commonly used string-handling functions */
 #include <pwd.h> // to get user name by user id
 #include <grp.h> // to get group name by group id
+#include <dirent.h> // read num of files in directory
 
 // https://pubs.opengroup.org/onlinepubs/009695299/functions/nftw.html
 // https://code.woboq.org/gcc/gcc/tree.c.html
 
 int direcory_counter = 0;
 int files_counter = 0;
+
+# define DT_REG 8
+# define DT_DIR 4
+// https://sites.uclouvain.be/SystInfo/usr/include/dirent.h.html
+
+int getNumOfFiles(char *pathname) {
+    int file_count = 0;
+    DIR * dirp;
+    struct dirent * entry;
+    dirp = opendir(pathname); /* There should be error handling after this */
+    while ((entry = readdir(dirp)) != NULL) {
+        /* If the entry is a regular file or dir */
+        if (entry->d_type == DT_REG || entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) 
+                file_count++;}}
+    closedir(dirp);
+    return file_count;
+}
+
 
 static int              /* Callback function called by ftw() */
 dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb) {
@@ -89,6 +109,8 @@ dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftw
     return 0;                                   /* Tell nftw() to continue */
     //return FTW_CONTINUE;
 }
+
+
 
 int
 main(int argc, char *argv[])
